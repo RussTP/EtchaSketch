ee6f02b817b38833bdce413ef17d3ce4925f19d7
RussTP <rpawlett@live.com>
2025-01-21 19:50:49 -0500

    modified loop statement to take row and column, to enable user to enter desired grid size, added button and prompt box to ask for grid size. Removed mouseonleave to get desired sketch-a-etch effect

--- javascript.js
+++ javascript.js
@@ -1,25 +1,43 @@
 const container = document.querySelector("#squareContainer");
+let column = 16;
+let row = 16;
 //loop to create 16x16 grid 
-for (let i = 0; i < 256; i++) {
+function gridCreate () {
+
+for (let i = 0; i < column * row; i++) {
     const square = document.createElement("div");
     square.id = "square" +i;
     square.className = "square";
     container.appendChild(square);
-    
-};
+
+
 
 //eventListener to change colour when mouse enters
-let colors = document.querySelectorAll(".square");
-colors.forEach((color) => {
-color.addEventListener("mouseenter", (e) => {
+
+square.addEventListener("mouseenter", (e) => {
 e.target.classList.add("color");
-})
-}) 
-//eventListener to remove colour when mouse leaves
-colors.forEach((color) => {
-    color.addEventListener("mouseleave", (e) =>{
-    e.target.classList.remove("color");   
-});
-});
+            });
+        } 
+    }
+
+    gridCreate();
+
+// rows * columns to create a new grid  
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    column = parseInt(prompt("Please enter the number of rows"));
+    row = parseInt(prompt("Please enter the number of columns"));
+    if (isNaN(row) || isNaN(column) || row < 1 || column < 1) {
+        alert("Please enter a valid amount of rows and columns");
+
+    }else {
+        container.innerHTML = "";   
+        const gridTotal = column * row;
+        gridCreate(gridTotal);
+    }
+    });
+
+
 
 
